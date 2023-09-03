ee15af3ffd7753c26f1f650a8c524650392d52ea
Lava <svanbergsdottir@gmail.com>
2023-09-03 12:15:52 -1000

    - Add first route - properly initialize the variable appRouter in src/App.js. Replace the string "REPLACE ME" with a new Route with the path /, that renders the element Root - In src/App.js, wrap the Route element created in the previous task with the createRoutesFromElements() function. - In src/App.js, wrap the call to createRoutesFromElements() from the previous exercise with createBrowserRouter()

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-
 
 // Add react-router-dom imports
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root /> }/>
+));
 
 function App() {
   return (
