b9512fbf5ad4537c28ccd9ef5288ac56bbaff725
Lava <svanbergsdottir@gmail.com>
2023-09-03 11:48:06 -1000

    - Setup - Install react-router-dom - Import React Router’s RouterProvider - Replace <p> with RouterProvider and define the attribute router with the value of appRouter - import createBrowserRouter, createRoutesFromElements and Route from 'react-router-dom' in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
 
 // Add react-router-dom imports
 
@@ -12,7 +14,7 @@ const appRouter = "REPLACE ME";
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
