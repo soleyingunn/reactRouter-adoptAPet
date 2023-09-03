8a5088b817b05bc2dda6d8548cb69870ecdd93a0
Lava <svanbergsdottir@gmail.com>
2023-09-03 12:26:34 -1000

    - Nested Routes and the Index Route - In src/App.js, where you defined the Route to render the Root element, add a nested Route that renders the HomePage component on the root path / - In src/components/root/index.js, import Outlet from react-router-dom. In the return statement of Root, add the Outlet component under the Navigation component.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } 
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={ <Root /> }/>
+  <Route path="/" element={ <Root /> }>
+    <Route index element={ <HomePage /> } />
+  </Route>
+
 ));
 
 function App() {
